f4c165f65fbe9fbfe5e02238e06a40ead23d48a3
Olya-Breskina <breskina-olya@mail.ru>
2022-12-23 20:15:36 +0300

    Initial commit

---
+++ src/test/java/Podgoretskaya/LoanConveyor/LoanConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package Podgoretskaya.LoanConveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanConveyorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
