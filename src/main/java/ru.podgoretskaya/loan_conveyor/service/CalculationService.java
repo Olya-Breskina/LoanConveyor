0f4559c78b503588174ae2b509c19cbf78cb1281
Olya-Breskina <breskina-olya@mail.ru>
2022-12-27 21:57:06 +0300

    нужно поправить методы Rate и PaymentScheduleElement, дописать  проверку входных данных (если она нужна)

--- src/main/java/ru.podgoretskaya/loan_conveyor/service/CalculationService.java
+++ src/main/java/ru.podgoretskaya/loan_conveyor/service/CalculationService.java
@@ -2,11 +2,15 @@ package ru.podgoretskaya.loan_conveyor.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.podgoretskaya.loan_conveyor.dto.CreditDTO;
+import ru.podgoretskaya.loan_conveyor.dto.LoanOfferDTO;
+import ru.podgoretskaya.loan_conveyor.dto.PaymentScheduleElement;
 import ru.podgoretskaya.loan_conveyor.dto.ScoringDataDTO;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,16 +21,24 @@ import static ru.podgoretskaya.loan_conveyor.dto.ScoringDataDTO.MaritalStatus.*;
 
 @Service
 public class CalculationService {
-    @Value("${initialRate}")
-    int rape;
+    // @Value("${initialRate}")
+    int rate = 20;
     @Value("${amountMin}")
     private BigDecimal amountMin;
+    @Value("${enabled}")
+    private int enabled;
+    @Value("${salaryClient}")
+    private int salaryClient;
+    @Value("${creditInsurance}")
+    private BigDecimal creditInsurance;
     boolean firstLastMiddleNameAnswer;
     boolean amountAnswer;
     boolean termAnswer;
     boolean birthdateAnswer;
     boolean passportAnswer;
     int age;
+    BigDecimal monthlyPayment;//ежемесячный платеж
+    BigDecimal psk;// полной стоимости кредита
 
     public boolean FirstLastMiddleNameCalculation(ScoringDataDTO model) {
         Pattern patlatletter = Pattern.compile("^[a-zA-Z]+$");
@@ -42,8 +54,8 @@ public class CalculationService {
             if ((lengthFirstName >= 2) && (lengthFirstName <= 30)
                     && (lengthLastName >= 2) && (lengthLastName <= 30)
                     && (lengthMiddleName >= 2) && (lengthMiddleName <= 30)
-                    && (firstNameLatLetter.matches() == true)
-                    && (lastNameLatLetter.matches() == true)
+                    && (firstNameLatLetter.matches())
+                    && (lastNameLatLetter.matches())
                     && (middleNameLatLetter.matches())
             ) {
                 firstLastMiddleNameAnswer = true;
@@ -52,8 +64,8 @@ public class CalculationService {
 
             if ((lengthFirstName >= 2) && (lengthFirstName <= 30)
                     && (lengthLastName >= 2) && (lengthLastName <= 30)
-                    && (firstNameLatLetter.matches() == true)
-                    && (lastNameLatLetter.matches() == true)
+                    && (firstNameLatLetter.matches())
+                    && (lastNameLatLetter.matches())
             ) {
                 firstLastMiddleNameAnswer = true;
             } else throw new IllegalArgumentException("проверьте ФИО");
@@ -94,7 +106,7 @@ public class CalculationService {
         return passportAnswer;
     }
 
-    public int Rape(ScoringDataDTO model) {
+    public int Rate(ScoringDataDTO model) {
 
         //Рабочий статус: Безработный → отказ;
         // Самозанятый → ставка увеличивается на 1;
@@ -103,37 +115,37 @@ public class CalculationService {
         if (UNEMPLOYED.equals(model.getEmployment())) {
             throw new IllegalArgumentException("отказ");
         } else if (SELF_EMPLOYED.equals(model.getEmployment())) {
-            rape = rape + 1;
+            rate = rate + 1;
         } else if (EMPLOYED.equals(model.getEmployment())) {
-            rape = rape;
+            rate = rate;
         } else if (BUSINESS_OWNER.equals(model.getEmployment())) {
-            rape = rape + 3;
+            rate = rate + 3;
         }
         //Позиция на работе: Менеджер среднего звена → ставка уменьшается на 2;
         // Топ-менеджер → ставка уменьшается на 4
         // владелец ставка-6
 
         if (WORKER.equals(model.getEmployment())) {
-            rape = rape;
+            rate = rate;
         } else if (MID_MANAGER.equals(model.getEmployment())) {
-            rape = rape - 2;
+            rate = rate - 2;
         } else if (TOP_MANAGER.equals(model.getEmployment())) {
-            rape = rape - 4;
+            rate = rate - 4;
         } else if (OWNER.equals(model.getEmployment())) {
-            rape = rape - 6;
+            rate = rate - 6;
         }
         //Семейное положение: Замужем/женат → ставка уменьшается на 3;
         // Разведен → ставка увеличивается на 1
         // одинок = ставка
         //вдова =ставка
         if (MARRIED.equals(model.getMaritalStatus())) {
-            rape = rape - 3;
+            rate = rate - 3;
         } else if (DIVORCED.equals(model.getMaritalStatus())) {
-            rape = rape + 1;
+            rate = rate + 1;
         } else if (SINGLE.equals(model.getMaritalStatus())) {
-            rape = rape;
+            rate = rate;
         } else if (WIDOW_WIDOWER.equals(model.getMaritalStatus())) {
-            rape = rape;
+            rate = rate;
         }
         // Возраст менее 20 или более 60 лет → отказ
         //Пол: Женщина, возраст от 35 до 60 лет → ставка уменьшается на 3; 20-35 ставка +1 
@@ -143,25 +155,86 @@ public class CalculationService {
             throw new IllegalArgumentException("отказ");
         }
         if ((MALE.equals(model.getGender())) && (age >= 30) && (age <= 55)) {
-            rape = rape;
+            rate = rate - 3;
         } else if ((MALE.equals(model.getGender())) && (age >= 20) && (age < 30) && (age > 55) && (age <= 60)) {
-            rape = rape + 1;
+            rate = rate + 1;
         } else if ((FEMALE.equals(model.getGender())) && (age >= 35) && (age <= 60)) {
-            rape = rape - 3;
+            rate = rate - 3;
         } else if ((FEMALE.equals(model.getGender())) && (age >= 20) && (age < 35)) {
-            rape = rape + 1;
+            rate = rate + 1;
         } else if (NOT_BINARY.equals(model.getGender())) {
-            rape = rape + 3;
+            rate = rate + 3;
+        }
+        //зарплатный клиент да-1%, нет+1%
+        if (model.getIsSalaryClient()) {
+            rate = rate - 1;
+        } else {
+            rate = rate + 1;
+        }
+        //страховка  да-3%, нет+3%
+        if ((model.getIsInsuranceEnabled())) {
+            rate = rate - 3;
+        } else {
+            rate = rate + 3;
         }
         //Сумма займа больше, чем 20 зарплат → отказ
-        BigDecimal twentySalaries = model.getEmployment().getSalary().multiply(BigDecimal.valueOf(20));
+      /* BigDecimal twentySalaries = model.getEmployment().getSalary().multiply(BigDecimal.valueOf(20));
         if (twentySalaries.compareTo(model.getAmount()) < 0) {
             throw new IllegalArgumentException("отказ");
         }
         //Стаж работы: Общий стаж менее 12 месяцев → отказ; Текущий стаж менее 3 месяцев → отказ
         if ((model.getEmployment().getWorkExperienceCurrent()<3)&&((model.getEmployment().getWorkExperienceTotal()<12))){
             throw new IllegalArgumentException("отказ");
+        }*/
+        return rate;
+    }
+
+    public BigDecimal MonthlyPaymentAmount(ScoringDataDTO model) {
+        BigDecimal requestedAmount = model.getAmount();//сумма кредита
+        Integer term = model.getTerm();//срок кредита
+        BigDecimal monthRate = BigDecimal.valueOf(rate).divide(BigDecimal.valueOf(1200), 5, RoundingMode.HALF_UP);// месячный %
+        BigDecimal exponentiation = (BigDecimal.valueOf(1).add(monthRate));//(1+monthRate)
+        BigDecimal degree = exponentiation.pow(term);
+        BigDecimal numerator = requestedAmount.multiply(monthRate.multiply(degree));
+        BigDecimal denominator = degree.subtract(BigDecimal.valueOf(1));
+        monthlyPayment = numerator.divide(denominator, 2, RoundingMode.HALF_UP); //ежемесячный платеж
+
+        return monthlyPayment;
+    }
+
+    public BigDecimal Psk(ScoringDataDTO model) {
+        BigDecimal requestedAmount = model.getAmount();
+        Integer term = model.getTerm();
+        boolean isInsuranceEnabled = model.getIsInsuranceEnabled();
+        BigDecimal totalAmountOfAllPayments;
+        if (isInsuranceEnabled) {
+            totalAmountOfAllPayments = monthlyPayment.multiply(BigDecimal.valueOf(term)).add(creditInsurance);
+        } else {
+            totalAmountOfAllPayments = monthlyPayment.multiply(BigDecimal.valueOf(term));
         }
-        return rape;
+
+        BigDecimal numerator = totalAmountOfAllPayments.divide(requestedAmount, 2, RoundingMode.HALF_UP)
+                .subtract(BigDecimal.valueOf(1));
+        BigDecimal termInYears = BigDecimal.valueOf(term).divide(BigDecimal.valueOf(12));//срок кредита в годах
+        psk = numerator.divide(termInYears).multiply(BigDecimal.valueOf(100));
+        return psk;
+    }
+  public List<PaymentScheduleElement> PaymentScheduleElement(ScoringDataDTO model){
+        List<PaymentScheduleElement> paymentScheduleElement = new ArrayList<>();
+      Integer number=0;number++;
+      LocalDate date=LocalDate.now();//дата
+      BigDecimal totalPayment=monthlyPayment;//всего к оплате
+      BigDecimal interestPayment;//выплата %
+      BigDecimal debtPayment=model.getAmount();//выплата долга
+      BigDecimal remainingDebt;//остаток
+        for ( number=1;  number<=model.getTerm(); number++){
+            number=number+1;
+            date=date.plusMonths(1);
+            interestPayment=debtPayment.multiply(BigDecimal.valueOf(rate).divide(BigDecimal.valueOf(1200), 5, RoundingMode.HALF_UP));
+            debtPayment=totalPayment.subtract(interestPayment);
+            remainingDebt=model.getAmount().subtract(debtPayment);
+            paymentScheduleElement.add(new PaymentScheduleElement(number, date,totalPayment,interestPayment,debtPayment,remainingDebt));
+        }
+        return paymentScheduleElement;
     }
 }
